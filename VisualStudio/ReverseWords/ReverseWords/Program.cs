54e1c5339fd1967be4780a9033b0f32ec5c4402a
Mark MacKenzie <mm800b@markmackenzie.com>
2014-09-24 19:14:25 -0400

    Added a method to reverse the words in a string
    This method involves splitting the original string with the string split
    method proving a parameter of an array of char that represents the
    characters to split on (space, tab character, etc).    The Append method
    of the StringBuilder within a for loop is then used to add each word
    followed by a space.  The space is only appended if it does not follow
    the last word.

--- VisualStudio/ReverseWords/ReverseWords/Program.cs
+++ VisualStudio/ReverseWords/ReverseWords/Program.cs
@@ -10,6 +10,29 @@ namespace ReverseWords
     {
         static void Main(string[] args)
         {
+            ReverseTheWordsOption1();
+        }
+
+        private static void ReverseTheWordsOption1()
+        {
+            char[] delimiterChars = { ' ', '\t' };
+            const string text = "The dog walks down the street";
+
+            var words = text.Split(delimiterChars);
+            var reversedWords = new StringBuilder();
+
+            for (int x = words.Count() - 1; x >= 0; x--)
+            {
+                reversedWords.Append(words[x]);
+
+                if (x > 0)
+                {
+                    reversedWords.Append(" ");
+                }
+            }
+
+            System.Console.WriteLine("Original text: '{0}'\n", text);
+            System.Console.WriteLine("Reversed text: '{0}'", reversedWords);
         }
     }
 }
